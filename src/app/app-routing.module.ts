468aad9c84c09b22f9e6e696fdfd552ab5cc0a73
Jhosef Marks de Carvalho <jhosef.carvalho@JVM60101279.local>
2019-01-21 19:14:13 -0200

    post-2: create route for customers

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   { path: 'home', loadChildren: './home/home.module#HomeModule' },
+  { path: 'customers', loadChildren: './customers/customers.module#CustomersModule' },
   { path: '', redirectTo: '/home', pathMatch: 'full'}
 ];
 
--- src/app/customers/customers-routing.module.ts
+++ src/app/customers/customers-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { CustomerListComponent } from './customer-list/customer-list.component';
+
+const routes: Routes = [
+  { path: '', component: CustomerListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
