6d9703714cf188b098a7638b78a2d16695e3ee95
Jhosef Marks de Carvalho <jhosef.carvalho@JVM60101279.local>
2019-01-28 19:46:30 -0200

    post-3: import FormsModule in SharedModule

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { ThfModule } from '@totvs/thf-ui';
 
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
+
     ThfModule
   ],
   exports: [
     CommonModule,
+    FormsModule,
+
     ThfModule
   ]
 })
