a43b91a5729dd9703e59c4ccda02e400957b6222
Jhosef Marks de Carvalho <jhosef.carvalho@JVM60101279.local>
2019-02-07 10:16:49 -0200

    post-4: update function save and add thf notification

--- src/app/customers/customer-form/customer-form.component.ts
+++ src/app/customers/customer-form/customer-form.component.ts
@@ -1,23 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+
+import { Subscription } from 'rxjs';
+
+import { ThfNotificationService } from '@totvs/thf-ui';
 
 @Component({
   selector: 'app-customer-form',
   templateUrl: './customer-form.component.html',
   styleUrls: ['./customer-form.component.css']
 })
-export class CustomerFormComponent implements OnInit {
+export class CustomerFormComponent implements OnDestroy, OnInit {
 
   private readonly url: string = 'https://sample-customers-api.herokuapp.com/api/thf-samples/v1/people';
+  private customerSub: Subscription;
 
   public customer: any = { };
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(
+    private thfNotification: ThfNotificationService,
+    private router: Router,
+    private httpClient: HttpClient) { }
+
+  ngOnDestroy() {
+    this.customerSub.unsubscribe();
+  }
 
   ngOnInit() { }
 
   save() {
-    this.httpClient.post(this.url, this.customer);
+    this.customerSub = this.httpClient.post(this.url, this.customer).subscribe(() => {
+      this.thfNotification.success('Cliente cadastrado com sucesso');
+
+      this.router.navigateByUrl('/customers');
+    });
   }
 
 }
