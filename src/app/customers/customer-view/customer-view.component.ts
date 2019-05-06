c569fb7856aac4e46b2b962b997a51bd03db696a
Jhosef Marks de Carvalho <jhosef.carvalho@JVM60101279.local>
2019-05-06 14:46:26 -0300

    post-5: get data from api

--- src/app/customers/customer-view/customer-view.component.ts
+++ src/app/customers/customer-view/customer-view.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ActivatedRoute } from '@angular/router';
+
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-customer-view',
   templateUrl: './customer-view.component.html',
   styleUrls: ['./customer-view.component.css']
 })
-export class CustomerViewComponent implements OnInit {
+export class CustomerViewComponent implements OnDestroy, OnInit {
 
-  constructor() { }
+  private readonly url: string = 'https://sample-customers-api.herokuapp.com/api/thf-samples/v1/people';
+
+  private customersSub: Subscription;
+  private paramsSub: Subscription;
+
+  constructor(private httpClient: HttpClient, private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.paramsSub = this.route.params.subscribe(params => this.loadData(params['id']));
+  }
+
+  ngOnDestroy() {
+    this.paramsSub.unsubscribe();
+    this.customersSub.unsubscribe();
+  }
+
+  private loadData(id) {
+    this.customersSub = this.httpClient.get(`${this.url}/${id}`).subscribe(console.log);
   }
 
 }
