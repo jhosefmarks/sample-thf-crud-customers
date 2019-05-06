2ba68a2b2a5f5d472e1f57348ad8f0092e413518
Jhosef Marks de Carvalho <jhosef.carvalho@JVM60101279.local>
2019-05-06 11:43:24 -0300

    post-5: add new component customer-view

---
+++ src/app/customers/customer-view/customer-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-customer-view',
+  templateUrl: './customer-view.component.html',
+  styleUrls: ['./customer-view.component.css']
+})
+export class CustomerViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
